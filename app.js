16bda5f618de44e23e47a8819f1c5ecc44259b30
Sukh Kalsi <sukhminder.kalsi@cohaesus.co.uk>
2016-07-14 19:07:05 +0100

    wit ai wip

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const MemoryDataStore = require('@slack/client').MemoryDataStore;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const token = process.env.SLACK_TOKEN || '';
 const isPhone = require('is-phone');
+const Wit = require('wit-js');
+
+const WitClient = new Wit.Client({
+  apiToken: "43WX7LZ3V7FIMD4HLWPTN2EAGMP7LYGF"
+});
+
+const processWitAi = (message) => {
+  WitClient.message(message, {}).then((response) => {
+    const entities = response.entities;
+    const from = entities.datetime[0].from;
+    const to = entities.datetime[0].to;
+    
+    console.log("Location is: ", entities.location[0].value);
+    console.log("Date from is: ", from.value)
+    console.log("Date to is: ", to.value)
+
+  }).catch((err) => {
+    console.error(err);
+  });
+}
 
 const rtm = new RtmClient(token, {
   logLevel: 'error',
@@ -29,26 +49,26 @@ const setState = (newState, stateResponse = false) => {
 const states = {
   DEFAULT: "DEFAULT",
   GET_NAME: "GET_NAME",
-  GET_ADDRESS: "GET_ADDRESS",
-  GET_PHONE: "GET_PHONE",
-  GET_GENDER: "GET_GENDER",
-  GET_EMAIL: "GET_EMAIL",
+  // GET_ADDRESS: "GET_ADDRESS",
+  // GET_PHONE: "GET_PHONE",
+  // GET_GENDER: "GET_GENDER",
+  // GET_EMAIL: "GET_EMAIL",
 }
 
-/* init state, set initial state */
+// /* init state, set initial state */
 let state;
 state = states.DEFAULT;
 
-/* functions that send responses and set state, called based on state */
+// /* functions that send responses and set state, called based on state */
 
 const handlers = {};
-const response = {
-    GET_NAME: "",
-    GET_ADDRESS: "",
-    GET_PHONE: "",
-    GET_GENDER: "",
-    GET_EMAIL: "",
-}
+// const response = {
+//     GET_NAME: "",
+//     GET_ADDRESS: "",
+//     GET_PHONE: "",
+//     GET_GENDER: "",
+//     GET_EMAIL: "",
+// }
 
 handlers.DEFAULT = (message) => {
   console.log(message.text, state);
@@ -57,52 +77,52 @@ handlers.DEFAULT = (message) => {
   console.log(message.text, state);
 }
 
-handlers.GET_NAME = (message) => {
-  console.log(message.text, state);
-  rtm.sendMessage("Ok, what's your address?", message.channel);
-  setState(states.GET_ADDRESS, message.text);
-  console.log(message.text, state);
-}
+// handlers.GET_NAME = (message) => {
+//   console.log(message.text, state);
+//   rtm.sendMessage("Ok, what's your address?", message.channel);
+//   setState(states.GET_ADDRESS, message.text);
+//   console.log(message.text, state);
+// }
 
-handlers.GET_ADDRESS = (message) => {
-  console.log(message.text, state);
-  rtm.sendMessage("What's your phone number?", message.channel);
-  setState(states.GET_PHONE, message.text);
-  console.log(message.text, state);
-}
+// handlers.GET_ADDRESS = (message) => {
+//   console.log(message.text, state);
+//   rtm.sendMessage("What's your phone number?", message.channel);
+//   setState(states.GET_PHONE, message.text);
+//   console.log(message.text, state);
+// }
 
-handlers.GET_PHONE = (message) => {
+// handlers.GET_PHONE = (message) => {
   
-  if (isPhone(message.text)) {
-    rtm.sendMessage("Almost there! What's your gender?", message.channel)
-    setState(states.GET_GENDER, message.text);
-  } else {
-    rtm.sendMessage("Oops. Please enter a valid number: (QQQ) QQQ-QQQQ", message.channel);
-  }
+//   if (isPhone(message.text)) {
+//     rtm.sendMessage("Almost there! What's your gender?", message.channel)
+//     setState(states.GET_GENDER, message.text);
+//   } else {
+//     rtm.sendMessage("Oops. Please enter a valid number: (QQQ) QQQ-QQQQ", message.channel);
+//   }
 
-  console.log(message.text, state);
-}
+//   console.log(message.text, state);
+// }
 
-handlers.GET_GENDER = (message) => {
-  console.log(message.text, state);
-  rtm.sendMessage("And finally...what's your email address?", message.channel)
-  setState(states.GET_EMAIL, message.text);
-  console.log(message.text, state);
-}
+// handlers.GET_GENDER = (message) => {
+//   console.log(message.text, state);
+//   rtm.sendMessage("And finally...what's your email address?", message.channel)
+//   setState(states.GET_EMAIL, message.text);
+//   console.log(message.text, state);
+// }
 
-handlers.GET_EMAIL = (message) => {
-  console.log(message.text, state);
-  setState(states.DEFAULT, message.text);
+// handlers.GET_EMAIL = (message) => {
+//   console.log(message.text, state);
+//   setState(states.DEFAULT, message.text);
 
-  rtm.sendMessage("All set!", message.channel);
-  rtm.sendMessage("Your response...", message.channel);
+//   rtm.sendMessage("All set!", message.channel);
+//   rtm.sendMessage("Your response...", message.channel);
   
-  for (var key in response) {
-    rtm.sendMessage(response[key], message.channel);
-  }
+//   for (var key in response) {
+//     rtm.sendMessage(response[key], message.channel);
+//   }
 
-  console.log(message.text, state);
-}
+//   console.log(message.text, state);
+// }
 
 const router = (message) => {
   handlers[state](message);
